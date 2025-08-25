415cf77c56f91df12cded487e1e0315130c4c377
its-harshitgoel <its.harshitgoel@gmail.com>
2025-08-25 08:21:02 +0530

    ex07

--- SOLID/ex07/src/Demo07.java
+++ SOLID/ex07/src/Demo07.java
@@ -1,7 +1,8 @@
 public class Demo07 {
     public static void main(String[] args) {
-        Machine m = new BasicPrinter();
-        m.print("Hello");
-        m.scan("/tmp/out"); // blows up
+        BasicPrinter printer = new BasicPrinter();
+        printer.print("Hello");
+        // m.scan("/tmp/out"); // blows up
+        //printer.scan("/tmp/out");
     }
 }
